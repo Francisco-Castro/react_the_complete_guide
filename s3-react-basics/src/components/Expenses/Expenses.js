4f0d7532bab63c832b92c365dd8ccce90295b565
Francisco Castro <catrapato@hotmail.com>
2023-06-26 10:26:33 -0400

    Add example illustrating computed (also known as derived) values

--- s3-react-basics/src/components/Expenses/Expenses.js
+++ s3-react-basics/src/components/Expenses/Expenses.js
@@ -2,12 +2,30 @@ import { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
-import "./Expenses.css"
+import "./Expenses.css";
 
-function Expenses({expenses}) {
-  const [filterYear, setFilteredYear] = useState("2019");
+function Expenses({ expenses }) {
+  const [filterYear, setFilteredYear] = useState("2021");
 
-  const dropdownChangeHandler = selectedYear => {
+  // Derived values AKA Computed values
+  let hiddenYears = "2019, 2021 and 2022";
+
+  switch (filterYear) {
+    case "2019":
+      hiddenYears = "2020, 2021 and 2022";
+      break;
+    case "2020":
+      hiddenYears = "2019, 2021 and 2022";
+      break;
+    case "2021":
+      hiddenYears = "2019, 2020 and 2022";
+      break;
+    default:
+      hiddenYears = "2019, 2020 and 2021";
+      break;
+  }
+
+  const dropdownChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
   return (
@@ -16,6 +34,7 @@ function Expenses({expenses}) {
         onChangeYear={dropdownChangeHandler}
         selectedYear={filterYear}
       />
+      <p>Years {hiddenYears} are hidden</p>
       {expenses.map(({ id, title, amount, date }) => {
         return (
           <ExpenseItem amount={amount} date={date} title={title} key={id} />
