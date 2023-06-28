4907748b47116a914f46d86e9b3b47c979f66d31
Francisco Castro <catrapato@hotmail.com>
2023-06-28 11:26:14 -0400

    Implement expense filtering

--- s3-react-basics/src/components/Expenses/Expenses.js
+++ s3-react-basics/src/components/Expenses/Expenses.js
@@ -5,41 +5,25 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses({ expenses }) {
-  const [filterYear, setFilteredYear] = useState("2021");
-
-  // Derived values AKA Computed values
-  let hiddenYears = "2019, 2021 and 2022";
-
-  switch (filterYear) {
-    case "2019":
-      hiddenYears = "2020, 2021 and 2022";
-      break;
-    case "2020":
-      hiddenYears = "2019, 2021 and 2022";
-      break;
-    case "2021":
-      hiddenYears = "2019, 2020 and 2022";
-      break;
-    default:
-      hiddenYears = "2019, 2020 and 2021";
-      break;
-  }
+  const [pickedYear, setPickedYear] = useState("2020");
 
   const dropdownChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
+    setPickedYear(selectedYear);
   };
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         onChangeYear={dropdownChangeHandler}
-        selectedYear={filterYear}
+        selectedYear={pickedYear}
       />
-      <p>Years {hiddenYears} are hidden</p>
-      {expenses.map(({ id, title, amount, date }) => {
-        return (
-          <ExpenseItem amount={amount} date={date} title={title} key={id} />
-        );
-      })}
+      {expenses
+        .filter((e) => e.date.getFullYear().toString() === pickedYear)
+        .map(({ id, title, amount, date }) => {
+          return (
+            <ExpenseItem amount={amount} date={date} title={title} key={id} />
+          );
+        })}
     </Card>
   );
 }
