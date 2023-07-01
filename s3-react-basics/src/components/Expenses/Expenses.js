ce92e80a167950560effa79cf3986e4276282ad4
Francisco Castro <catrapato@hotmail.com>
2023-06-30 21:45:34 -0400

    Add logic to render special msg when list of expenses is empty

--- s3-react-basics/src/components/Expenses/Expenses.js
+++ s3-react-basics/src/components/Expenses/Expenses.js
@@ -5,11 +5,23 @@ import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import "./Expenses.css";
 
 function Expenses({ expenses }) {
-  const [pickedYear, setPickedYear] = useState("2020");
+  const [pickedYear, setPickedYear] = useState("2019");
 
   const dropdownChangeHandler = (selectedYear) => {
     setPickedYear(selectedYear);
   };
+
+  const filteredExpenses = expenses.filter(
+    (e) => e.date.getFullYear().toString() === pickedYear
+  );
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map(({ id, title, amount, date }) => {
+      return <ExpenseItem amount={amount} date={date} title={title} key={id} />;
+    });
+  }
 
   return (
     <Card className="expenses">
@@ -17,13 +29,7 @@ function Expenses({ expenses }) {
         onChangeYear={dropdownChangeHandler}
         selectedYear={pickedYear}
       />
-      {expenses
-        .filter((e) => e.date.getFullYear().toString() === pickedYear)
-        .map(({ id, title, amount, date }) => {
-          return (
-            <ExpenseItem amount={amount} date={date} title={title} key={id} />
-          );
-        })}
+      {expensesContent}
     </Card>
   );
 }
