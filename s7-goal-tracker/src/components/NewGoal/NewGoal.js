aaf14728ed29fb60b69a1c504c888ab8cbadd9ca
Francisco Castro <catrapato@hotmail.com>
2023-07-09 23:54:24 -0400

    Add styling for invalid input

--- s7-goal-tracker/src/components/NewGoal/NewGoal.js
+++ s7-goal-tracker/src/components/NewGoal/NewGoal.js
@@ -1,25 +1,35 @@
 import Card from "../UI/Card";
 import { useState } from "react";
-
 import "./NewGoal.css";
+
 const NewGoal = (props) => {
   const [goal, setGoal] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setGoal(event.target.value);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
+    if (goal.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onSaveGoal(goal);
-
-    setGoal("")
+    setGoal("");
   };
 
   return (
     <Card className="new-card">
-      <form onSubmit={submitHandler} className="form">
+      <form
+        onSubmit={submitHandler}
+        className={`form-control ${!isValid && "invalid"}`}
+      >
         <label>Course Goal</label>
         <input onChange={goalChangeHandler} type="text" value={goal}></input>
         <button type="submit">Add Goal</button>
